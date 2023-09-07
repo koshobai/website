786b097e903eae05a6dd027aac35dafffa1c472e
Tariq Lacy <hello@koshobai.com>
2023-09-07 17:19:58 +0900

    attempting to resolve image rendering issue

--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
