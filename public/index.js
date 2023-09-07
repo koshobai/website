9dd5c441445c4ae65212aa1c3011a8931998e63c
Tariq Lacy <hello@koshobai.com>
2023-09-07 13:20:27 +0900

    update gitignore and package.json

---
+++ public/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFWUAkzGXEF_vLLx6L4NaCCtqxGWtWx-A',
+  authDomain: 'koshobai-learning.firebaseapp.com',
+  projectId: 'koshobai-learning',
+  storageBucket: 'koshobai-learning.appspot.com',
+  messagingSenderId: '1020524338036',
+  appId: '1:1020524338036:web:c97a14685e3380b5fb6a21',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
